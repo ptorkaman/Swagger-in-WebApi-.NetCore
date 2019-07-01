414cc2abe4e2b5ae3196dcbca04de5d98167ce0e
Parvin Torkaman <32588995+ptorkaman@users.noreply.github.com>
2019-07-01 10:56:22 +0430

    Add files via upload

---
+++ SwaggerWebApiNetCore/SwaggerWebApiNetCore/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SwaggerWebApiNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        const string SWAGGER_DOC_NAME = "v1";
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My API Swagger",
+                    Description = "My First ASP.NET Core Web API Swagger",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Talking Dotnet", Email = "YourMail.com", Url = "www.YourURL.com" }
+                });
+            });
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            // setup swaggerUI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                url: $"/swagger/{SWAGGER_DOC_NAME}/swagger.json",
+                name: "xxx API"
+                );
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
